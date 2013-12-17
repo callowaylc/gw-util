2e67d2b5151685ca3b73d016f251a935184853e1
Ubuntu <ubuntu@ip-10-239-12-50.ec2.internal>
2013-12-17 19:05:28 +0000

    Updated sync script to autopush

--- sync.rb
+++ sync.rb
@@ -1,22 +1,63 @@
 #!/usr/bin/env ruby
 # Author:      Christian Calloway callowaylc@gmail
-# Description: Responsible for syncing 
+# Description: Responsible for syncing applications against bare repo
 
 
 ### REQUIRES
 
-require 'mysql2'
-require 'csv'
+require 'git'
+require 'date'
 
 ### CONSTANTS 
 
 DIR_APPLICATIONS = '/var/www'
 
+### FUNCTIONS
+
 
 ### MAIN
 
+
 # iterate through directories 
 Pathname.glob(DIR_APPLICATIONS + '/*/').map(&:basename).each do | directory | 
-	puts directory
-	exit
+
+	# ensure that directory is git enabled
+	begin
+		git     = Git.open   directory
+		logdate = Date.parse git.log.first.date 
+
+	else
+		raise "#{directory} is not a git repo!"
+
+	end
+
+	# change to application directory
+	Dir.chdir directory
+
+
+	# check if files have been updated since last run; only run
+	# if last git time 
+	file = nil
+
+	Dir.glob("**/*").each do | path |
+
+		# if file has been updated, then we need to flag that
+		# an update/push must occur
+		file = File.new path
+
+		# break from block if file update time is greater than 
+		# last log update time
+		break if Date.parse(file.mtime) > logdate
+	end 
+
+	# lets check again if last file exceeds logdate; if the case we
+	# need to fire off a push to master
+	if Date.parse(file.mtime) > logdate
+		git.commmit_all "Automatic REPO update #{file.mtime}"
+		git.push(g.remote 'name')
+	end
+
+	
+
+
 end
